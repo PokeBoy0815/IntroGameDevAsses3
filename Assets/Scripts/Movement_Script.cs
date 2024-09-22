378f80981f38222bc571155a00735772bc615507
Markus Wei****ger <markus.weinberger@ur.de>
2024-09-22 23:05:38 +1000

    fixed and finalized movement

--- Assets/Scripts/Movement_Script.cs
+++ Assets/Scripts/Movement_Script.cs
@@ -6,8 +6,10 @@ public class Movement_Script : MonoBehaviour
 {
     [SerializeField] 
     private GameObject player;
+
+    private Animator player_animator;
     
-    private List<Vector3> TARGETS= new List<Vector3>{new Vector3(-12.5f, 9.5f, 0f), new Vector3(-7.5f, 9.5f, 0f), new Vector3(-7.5f, 13.5f, 0f), new Vector3(-12.5f, 13.5f, 0f)};
+    private List<Vector3> TARGETS= new List<Vector3>{new Vector3(-7.5f, 9.5f, 0f), new Vector3(-7.5f, 13.5f, 0f), new Vector3(-12.5f, 13.5f, 0f), new Vector3(-12.5f, 9.5f, 0f)};
     private float DURATION_HORIZONTAL = 3;
     //private float DURATION_VERTICAL = 2;
 
@@ -22,25 +24,27 @@ public class Movement_Script : MonoBehaviour
     void Start()
     {
         StartTime = Time.time;
-        EndPosition = TARGETS[0];
-        StartPosition = player.transform.position;
         Direction = 0;
+        EndPosition = TARGETS[Direction];
+        StartPosition = player.transform.position;
         Duration = DURATION_HORIZONTAL;
+        player_animator = player.GetComponent<Animator>();
+        player_animator.SetInteger("Direction", Direction);
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(player.transform.position, EndPosition) > 0.01f)
+        if (Vector3.Distance(player.transform.position, EndPosition) > 0.1f)
         { 
             player.transform.position = Vector3.Lerp(StartPosition, EndPosition, (Time.time-StartTime) / Duration);
         }
         
-        if (Vector3.Distance(player.transform.position, EndPosition) <= 0.01f)
+        if (Vector3.Distance(player.transform.position, EndPosition) <= 0.1f)
         { 
             player.transform.position = EndPosition;
-            if (Direction != 3)
+            if (Direction < 3)
             {
                 Direction++;
             }
@@ -52,6 +56,7 @@ public class Movement_Script : MonoBehaviour
             StartTime = Time.time;
             StartPosition = EndPosition;
             EndPosition = TARGETS[Direction];
+            player_animator.SetInteger("Direction", Direction);
             Debug.Log(EndPosition);
         }
         
